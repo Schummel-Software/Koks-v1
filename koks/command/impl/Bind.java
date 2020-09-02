5ee055fce8a3e1258cf833334af4d0eed916b2f5
Bryan <bryanshandy0@gmail.com>
2020-09-02 22:17:23 +0200

    Added BindCommand

---
+++ koks/command/impl/Bind.java
@@ -0,0 +1,34 @@
+package koks.command.impl;
+
+import koks.Koks;
+import koks.command.Command;
+import koks.modules.Module;
+import org.lwjgl.input.Keyboard;
+
+/**
+ * @author avox | lmao | kroko
+ * @created on 02.09.2020 : 22:01
+ */
+public class Bind extends Command {
+
+    public Bind() {
+        super("Bind", "SetKey");
+    }
+
+    @Override
+    public void execute(String[] args) {
+        if (args.length != 2)
+            return;
+
+        String moduleArg = args[0].toUpperCase();
+        String keyArg = args[1].toUpperCase();
+
+        for (Module module : Koks.getKoks().moduleManager.getModules()) {
+            if (module.getModuleName().equalsIgnoreCase(moduleArg)) {
+                module.setKeyBind(Keyboard.getKeyIndex(keyArg));
+                sendmsg("§fSet Key of §b§l" + moduleArg + " §fto §b§l" + Keyboard.getKeyName(Keyboard.getKeyIndex(keyArg)) + "§f", true);
+            }
+        }
+    }
+
+}
