3eaf0231492039f27c3d59cefb70263c7b91f397
David <geltrix90@gmail.com>
2020-09-02 23:10:29 +0200

    reee

--- koks/modules/impl/movement/Speed.java
+++ koks/modules/impl/movement/Speed.java
@@ -18,7 +18,15 @@ public class Speed extends Module {
 
     @Override
     public void onEvent(Event event) {
-
+        if (event instanceof EventUpdate) {
+            if (mc.thePlayer.moveForward != 0 && !mc.gameSettings.keyBindBack.isKeyDown()) {
+                if (mc.thePlayer.onGround) {
+                    mc.thePlayer.motionY = 0.42;
+                } else {
+                    MovementUtil.setSpeed(0.4);
+                }
+            }
+        }
     }
 
     @Override
--- koks/utilities/MovementUtil.java
+++ koks/utilities/MovementUtil.java
@@ -9,5 +9,27 @@ import net.minecraft.util.MathHelper;
  */
 public class MovementUtil {
 
+    public static void setSpeed(double speed) {
+
+        double strafeSpeed = speed;
+        float forward = Minecraft.getMinecraft().thePlayer.movementInput.moveForward;
+        float strafe = Minecraft.getMinecraft().thePlayer.movementInput.moveStrafe;
+
+        if (forward > 0 && strafe > 0) {
+            forward = 1;
+            if(strafe > 0)
+                strafeSpeed = speed / 2;
+        } else {
+            Minecraft.getMinecraft().thePlayer.motionX = 0;
+            Minecraft.getMinecraft().thePlayer.motionZ = 0;
+        }
+
+        float f1 = MathHelper.sin(Minecraft.getMinecraft().thePlayer.rotationYaw * (float) Math.PI / 180.0F);
+        float f2 = MathHelper.cos(Minecraft.getMinecraft().thePlayer.rotationYaw * (float) Math.PI / 180.0F);
+
+        Minecraft.getMinecraft().thePlayer.motionX = (double) (strafe * strafeSpeed * f2 - forward * speed * f1);
+        Minecraft.getMinecraft().thePlayer.motionZ = (double) (forward * speed * f2 + strafe * strafeSpeed * f1);
+
+    }
 
 }
