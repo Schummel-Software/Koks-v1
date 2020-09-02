1d81d12d5422db759de216dd1bec2aa9ff9a6888
David <geltrix90@gmail.com>
2020-09-02 21:43:21 +0200

    RE

--- koks/utilities/MovementUtil.java
+++ koks/utilities/MovementUtil.java
@@ -10,10 +10,20 @@ import net.minecraft.util.MathHelper;
 public class MovementUtil {
 
     public static void setSpeed(double speed) {
-        float forward = Minecraft.getMinecraft().thePlayer.moveForward;
-        float strafe = Minecraft.getMinecraft().thePlayer.moveStrafing;
+        float forward = Minecraft.getMinecraft().thePlayer.movementInput.moveForward;
+        float strafe = Minecraft.getMinecraft().thePlayer.movementInput.moveStrafe;
+
+        if (forward > 1 || strafe > 1) {
+            forward = 1F;
+            strafe = 1F;
+        } else {
+            Minecraft.getMinecraft().thePlayer.motionX = 0F;
+            Minecraft.getMinecraft().thePlayer.motionZ = 0F;
+        }
+
         float f1 = MathHelper.sin(Minecraft.getMinecraft().thePlayer.rotationYaw * (float) Math.PI / 180.0F);
         float f2 = MathHelper.cos(Minecraft.getMinecraft().thePlayer.rotationYaw * (float) Math.PI / 180.0F);
+        
         Minecraft.getMinecraft().thePlayer.motionX += (double) (strafe * speed * f2 - forward * speed * f1);
         Minecraft.getMinecraft().thePlayer.motionZ += (double) (forward * speed * f2 + strafe * speed * f1);
     }
