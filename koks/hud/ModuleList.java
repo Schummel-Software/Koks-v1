2c0f796cf39ae4324c1bb310ebcc7a6d0c31334b
David <geltrix90@gmail.com>
2020-09-04 05:04:04 +0200

    Added Sorting

--- koks/hud/ModuleList.java
+++ koks/hud/ModuleList.java
@@ -7,6 +7,8 @@ import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.ScaledResolution;
 
+import java.util.Comparator;
+
 /**
  * @author avox | lmao | kroko
  * @created on 02.09.2020 : 22:55
@@ -18,15 +20,16 @@ public class ModuleList {
 
     public void drawList() {
         ScaledResolution sr = new ScaledResolution(mc);
-        int y = 0;
-        for (Module module : Koks.getKoks().moduleManager.getModules()) {
+        int[] y = {0};
 
+        Koks.getKoks().moduleManager.getModules().stream().sorted(Comparator.comparingDouble(value -> -Minecraft.getMinecraft().fontRendererObj.getStringWidth(value.getModuleName()))).forEach(module -> {
             if (module.isToggled() && module.isVisible()) {
-                Gui.drawRect(sr.getScaledWidth() - fr.getStringWidth(module.getModuleName()) - 4, y, sr.getScaledWidth(), y + fr.FONT_HEIGHT + 1, Integer.MIN_VALUE);
-                fr.drawStringWithShadow(module.getModuleName(), sr.getScaledWidth() - fr.getStringWidth(module.getModuleName()) - 2, y + 1, 0xFFFFFFFF);
-                y += fr.FONT_HEIGHT + 1;
+                Gui.drawRect(sr.getScaledWidth() - fr.getStringWidth(module.getModuleName()) - 4, y[0], sr.getScaledWidth(), y[0] + fr.FONT_HEIGHT + 1, Integer.MIN_VALUE);
+                fr.drawStringWithShadow(module.getModuleName(), sr.getScaledWidth() - fr.getStringWidth(module.getModuleName()) - 2, y[0] + 1, 0xFFFFFFFF);
+                y[0] += fr.FONT_HEIGHT + 1;
             }
-        }
+        });
+
     }
 
 }
