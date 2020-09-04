04c77d4a6b37206d087549dddbabf2ee8e7be0ca
KrokoOnAir <krokoonair@gmail.com>
2020-09-04 17:59:31 +0200

    added disabler mode

--- koks/modules/impl/utilities/Disabler.java
+++ koks/modules/impl/utilities/Disabler.java
@@ -3,6 +3,7 @@ package koks.modules.impl.utilities;
 import koks.event.Event;
 import koks.event.impl.PacketEvent;
 import koks.modules.Module;
+import koks.utilities.value.values.ModeValue;
 import net.minecraft.client.Minecraft;
 import net.minecraft.network.play.server.S32PacketConfirmTransaction;
 
@@ -12,19 +13,24 @@ import net.minecraft.network.play.server.S32PacketConfirmTransaction;
  */
 public class Disabler extends Module {
 
+    public ModeValue<String> mode = new ModeValue<String>("Mode", "Hypixel", new String[]{"Hypixel"}, this);
+
     public Disabler() {
         super("Disabler", Category.UTILITIES);
+        addValue(mode);
     }
 
     @Override
     public void onEvent(Event event) {
         if (event instanceof PacketEvent) {
-            if (((PacketEvent) event).getType() == PacketEvent.Type.RECIVE) {
-                if (Minecraft.getMinecraft().getCurrentServerData().serverIP.contains("hypixel") && Minecraft.getMinecraft().theWorld != null && Minecraft.getMinecraft().thePlayer != null) {
-                    if (((PacketEvent) event).getPacket() instanceof S32PacketConfirmTransaction) {
-                        final S32PacketConfirmTransaction s32PacketConfirmTransaction = (S32PacketConfirmTransaction) ((PacketEvent) event).getPacket();
-                        if (s32PacketConfirmTransaction.getActionNumber() < 0) {
-                            event.setCanceled(true);
+            if (mode.getSelectedMode().equalsIgnoreCase("Hypixel")) {
+                if (((PacketEvent) event).getType() == PacketEvent.Type.RECIVE) {
+                    if (Minecraft.getMinecraft().getCurrentServerData().serverIP.contains("hypixel") && Minecraft.getMinecraft().theWorld != null && Minecraft.getMinecraft().thePlayer != null) {
+                        if (((PacketEvent) event).getPacket() instanceof S32PacketConfirmTransaction) {
+                            final S32PacketConfirmTransaction s32PacketConfirmTransaction = (S32PacketConfirmTransaction) ((PacketEvent) event).getPacket();
+                            if (s32PacketConfirmTransaction.getActionNumber() < 0) {
+                                event.setCanceled(true);
+                            }
                         }
                     }
                 }
