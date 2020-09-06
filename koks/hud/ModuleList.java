7ccba1431bebac7e52f7d1a6a09155fd5200879e
David <geltrix90@gmail.com>
2020-09-06 11:22:38 +0200

    Added Shadow into Arraylist with an Option to disable it

--- koks/hud/ModuleList.java
+++ koks/hud/ModuleList.java
@@ -4,11 +4,15 @@ import koks.Koks;
 import koks.modules.Module;
 import koks.modules.impl.utilities.HUD;
 import koks.modules.impl.visuals.ClearTag;
+import koks.utilities.RenderUtils;
 import koks.utilities.value.values.BooleanValue;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.ScaledResolution;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.util.ResourceLocation;
+import org.lwjgl.opengl.GL11;
 
 import java.util.Comparator;
 
@@ -20,18 +24,47 @@ public class ModuleList {
 
     private final Minecraft mc = Minecraft.getMinecraft();
     private final FontRenderer fr = Minecraft.getMinecraft().fontRendererObj;
+    private final RenderUtils renderUtils = new RenderUtils();
 
     public void drawList(boolean shadow) {
         ScaledResolution sr = new ScaledResolution(mc);
         int[] y = {0};
 
         Koks.getKoks().moduleManager.getModules().stream().filter(Module::isToggled).sorted(Comparator.comparingDouble(module -> -Minecraft.getMinecraft().fontRendererObj.getStringWidth(Koks.getKoks().moduleManager.getModule(ClearTag.class).isToggled() ? module.getModuleName() : module.getNameForArrayList()))).forEach(module -> {
-            if(module.isVisible()) {
+            if (module.isVisible()) {
                 if (Koks.getKoks().moduleManager.getModule(ClearTag.class).isToggled()) {
-                    Gui.drawRect(sr.getScaledWidth() - fr.getStringWidth(module.getModuleName()) - 4, y[0], sr.getScaledWidth(), y[0] + fr.FONT_HEIGHT + 1, Integer.MIN_VALUE);
+
+                    if(shadow) {
+                        GL11.glPushMatrix();
+                        GlStateManager.disableAlpha();
+                        GlStateManager.enableBlend();
+                        GL11.glEnable(GL11.GL_BLEND);
+                        GL11.glColor4f(1, 1, 1, 1);
+                        renderUtils.drawImage(new ResourceLocation("client/shadows/arraylistshadow.png"), sr.getScaledWidth() - fr.getStringWidth(module.getModuleName()) - 4, y[0], fr.getStringWidth(module.getModuleName()), 15, false);
+                        GL11.glDisable(GL11.GL_BLEND);
+                        GlStateManager.enableAlpha();
+                        GlStateManager.disableBlend();
+                        GL11.glPopMatrix();
+                    }else{
+                        Gui.drawRect(sr.getScaledWidth() - fr.getStringWidth(module.getModuleName()) - 4, y[0], sr.getScaledWidth(), y[0] + fr.FONT_HEIGHT + 1, Integer.MIN_VALUE);
+                    }
                     fr.drawStringWithShadow(module.getModuleName(), sr.getScaledWidth() - fr.getStringWidth(module.getModuleName()) - 2, y[0] + 1, Koks.getKoks().client_color.getRGB());
                 } else {
-                    Gui.drawRect(sr.getScaledWidth() - fr.getStringWidth(module.getNameForArrayList()) - 4, y[0], sr.getScaledWidth(), y[0] + fr.FONT_HEIGHT + 1, Integer.MIN_VALUE);
+
+                    if(shadow) {
+                        GL11.glPushMatrix();
+                        GlStateManager.disableAlpha();
+                        GlStateManager.enableBlend();
+                        GL11.glEnable(GL11.GL_BLEND);
+                        GL11.glColor4f(1, 1, 1, 1);
+                        renderUtils.drawImage(new ResourceLocation("client/shadows/arraylistshadow.png"), sr.getScaledWidth() - fr.getStringWidth(module.getNameForArrayList()) - 4, y[0], fr.getStringWidth(module.getNameForArrayList()), 15, false);
+                        GL11.glDisable(GL11.GL_BLEND);
+                        GlStateManager.enableAlpha();
+                        GlStateManager.disableBlend();
+                        GL11.glPopMatrix();
+                    }else{
+                        Gui.drawRect(sr.getScaledWidth() - fr.getStringWidth(module.getNameForArrayList()) - 4, y[0], sr.getScaledWidth(), y[0] + fr.FONT_HEIGHT + 1, Integer.MIN_VALUE);
+                    }
                     fr.drawStringWithShadow(module.getNameForArrayList(), sr.getScaledWidth() - fr.getStringWidth(module.getNameForArrayList()) - 2, y[0] + 1, Koks.getKoks().client_color.getRGB());
                 }
                 y[0] += fr.FONT_HEIGHT + 1;
